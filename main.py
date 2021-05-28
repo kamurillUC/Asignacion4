518e4279df4146b94b321ec6a4dd38609b94130d
kamurillUC <84762384+kamurillUC@users.noreply.github.com>
2021-05-28 01:07:56 -0600

    Complete code

---
+++ main.py
@@ -0,0 +1,42 @@
+from mis_funciones import *
+#agregarEstudiantes()
+#iimprimirListaBid(fListaBid)
+#imprimirBisiesto()
+#limpiarConsola()
+
+bandera = 0
+respuesta = 0
+estudiantes = []
+
+limpiarConsola()
+while bandera == 0:    
+    print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+    print(f"Menú de opciones:")
+    print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+    print(f"1. Agregar estudiantes al grupo\n2. Ver estudiantes del grupo\n3. Encontrar si un año es bisiesto\n4. Salir")    
+    respuesta = input(">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><\nDigite su opción: ")
+    if respuesta == "1":  
+        limpiarConsola()
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        print(f"1. Agregar estudiantes al grupo")
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        estudiantes = agregarEstudiantes()  
+    elif respuesta == "2":
+        limpiarConsola()
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        print(f"2. Ver estudiantes del grupo")
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        imprimirListaBid(estudiantes)
+    elif respuesta == "3":
+        limpiarConsola()
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        print(f"3. Encontrar si un año es bisiesto")
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        imprimirBisiesto()
+    else:
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        print(f"GitHub: kamurillUC")
+        print(f">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+        break
+
+
---
+++ mis_funciones.py
@@ -0,0 +1,77 @@
+#Funciones del Diagrama1
+def agregarEstudiantes():
+
+    CUPO = 3
+    estudiantes = []    
+    nombre = ""
+    email = ""
+    x = 0
+    y = 0   
+    respuesta = ""
+    
+    for x in range(CUPO):
+        nombre = input('Por favor digite su nombre: ')
+        correo = input('Por favor digite su correo: ')                
+        estudiantes.append([nombre, correo])
+        if(x != CUPO-1):         
+            respuesta = input("Agregar otro estudiante? (Si/No)")
+            if respuesta.lower() == "no":
+                break
+        else:
+            print("El cupo esta lleno")
+            break
+    return estudiantes
+
+def imprimirListaBid(fListaBid):
+    lenLista = len(fListaBid)
+    print("-----------------------------------------------------------") 
+    print(">< >< >< >< >< >< ><Lista de estudiantes>< >< >< >< >< >< >")
+    print("-----------------------------------------------------------")
+    if lenLista > 0:    
+        for x in range(lenLista):
+            print(f"Estudiante #{x+1}\nNombre:{fListaBid[x][0]}\nCorreo: {fListaBid[x][1]}\n")
+        
+    else:
+        print("No hay estudiantes agregados")
+    print("-----------------------------------------------------------")    
+
+
+#Funcion del Diagrama2
+
+def identifBisiesto(n):    
+    if n % 4 == 0:
+        if n % 100 == 0 and n % 400 == 0:
+            return True        
+    else:
+        return False
+
+
+def identifInt(var):
+    if var[0] == ('-', '+'):
+        return var[1:].isdigit()
+    else:
+        return var.isdigit()
+
+def imprimirBisiesto():
+    bandera = 0
+    while bandera == 0:
+        respuesta = input("Ingresar año: ")
+        if identifInt(respuesta) == False:
+            print(">< >< >< >< >< Por favor ingresar un número >< >< >< >< ><")                   
+        else:
+            respuesta = int(respuesta)
+            if identifBisiesto(respuesta):
+                print("El año es bisiesto")
+                break
+            else:
+                print("El año no es bisiesto")
+                break
+
+
+#Funcion para limpiar consola segun OS
+def limpiarConsola():
+    import os
+    if os.name == "posix" or os.name == "mac":
+        os.system("clear")
+    elif os.name == "ce" or os.name == "dos" or os.name == "nt":
+        os.system("cls")
